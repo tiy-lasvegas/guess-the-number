433bc09783e09112517428908c9aaa7be42938f9
Michael Sweeney <Michael@overthemike.local>
2015-11-04 16:13:39 -0800

    added main.js stuff for gh-pages

---
+++ main.js
@@ -0,0 +1,20 @@
+var correct = Math.ceil(Math.random()*10);
+var form = document.querySelector('form');
+var input = document.querySelector('input');
+var response = document.querySelector('.response');
+
+form.addEventListener('submit', checkGuess);
+
+function checkGuess (event) {
+  event.preventDefault();
+  var guess = Number(input.value);
+  if (guess === correct) {
+    response.textContent = "Correct!!!";
+  }
+  else if (guess > correct) {
+    response.textContent = guess + " is too high!";
+  }
+  else if (guess < correct) {
+    response.textContent = guess + " is too low!";
+  }
+}
